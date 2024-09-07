0d4270691d97012ffb3bf40ecd8228ec8daa2e4f
Anurag kumar <anuragk.c11@gmail.com>
2024-09-07 21:31:28 +0530

    navbar fix

--- ecom/app/static/app/js/myscript.js
+++ ecom/app/static/app/js/myscript.js
@@ -22,72 +22,72 @@ $('#slider1, #slider2, #slider3').owlCarousel({
     }
 })
 
-$('.plus-cart').click(function(){
+$('.plus-cart').click(function () {
     console.log("Button clicked")
     var id = $(this).attr("pid").toString();
     var eml = this.parentNode.children[2]
-    console.log(eml) 
+    console.log(eml)
     $.ajax({
-        type:"GET",
-        url:"/pluscart/",
-        data:{
+        type: "GET",
+        url: "/pluscart/",
+        data: {
             prod_id: id
         },
-        success:function(data){
-            console.log("data = ",data);
-            eml.innerText = data.quantity 
-            document.getElementById("amount").innerText=data.amount 
-            document.getElementById("totalamount").innerText=data.totalamount
+        success: function (data) {
+            console.log("data = ", data);
+            eml.innerText = data.quantity
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("totalamount").innerText = data.totalamount
         }
     })
 })
 
-$('.minus-cart').click(function(){
-    var id=$(this).attr("pid").toString();
-    var eml=this.parentNode.children[2] 
+$('.minus-cart').click(function () {
+    var id = $(this).attr("pid").toString();
+    var eml = this.parentNode.children[2]
     $.ajax({
-        type:"GET",
-        url:"/minuscart/",
-        data:{
-            prod_id:id
+        type: "GET",
+        url: "/minuscart/",
+        data: {
+            prod_id: id
         },
-        success:function(data){
-            eml.innerText=data.quantity 
-            document.getElementById("amount").innerText=data.amount 
-            document.getElementById("totalamount").innerText=data.totalamount
+        success: function (data) {
+            eml.innerText = data.quantity
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("totalamount").innerText = data.totalamount
         }
     })
 })
 
 
-$('.remove-cart').click(function(){
-    var id=$(this).attr("pid").toString();
-    var eml=this
+$('.remove-cart').click(function () {
+    var id = $(this).attr("pid").toString();
+    var eml = this
     $.ajax({
-        type:"GET",
-        url:"/removecart/",
-        data:{
-            prod_id:id
+        type: "GET",
+        url: "/removecart/",
+        data: {
+            prod_id: id
         },
-        success:function(data){
-            document.getElementById("amount").innerText=data.amount 
-            document.getElementById("totalamount").innerText=data.totalamount
-            eml.parentNode.parentNode.parentNode.parentNode.remove() 
+        success: function (data) {
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("totalamount").innerText = data.totalamount
+            eml.parentNode.parentNode.parentNode.parentNode.remove()
         }
     })
 })
 
 
-$('.plus-wishlist').click(function(){
-    var id=$(this).attr("pid").toString();
+$('.plus-wishlist').click(function () {
+    var id = $(this).attr("pid").toString();
     console.log(id)
     $.ajax({
-        type:"GET",
-        url:"/pluswishlist/",
-        data:{
-            prod_id:id
+        type: "GET",
+        url: "/pluswishlist/",
+        data: {
+            prod_id: id
         },
-        success:function(data){
+        success: function (data) {
             //alert(data.message)
             window.location.href = `http://localhost:8000/product-detail/${id}`
         }
@@ -95,15 +95,15 @@ $('.plus-wishlist').click(function(){
 })
 
 
-$('.minus-wishlist').click(function(){
-    var id=$(this).attr("pid").toString();
+$('.minus-wishlist').click(function () {
+    var id = $(this).attr("pid").toString();
     $.ajax({
-        type:"GET",
-        url:"/minuswishlist/",
-        data:{
-            prod_id:id
+        type: "GET",
+        url: "/minuswishlist/",
+        data: {
+            prod_id: id
         },
-        success:function(data){
+        success: function (data) {
             window.location.href = `http://localhost:8000/product-detail/${id}`
         }
     })
